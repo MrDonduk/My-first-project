545e1a4b77daf3d774747994afcb4778238ecfdd
MrDonduk <vladmrdondu2409@gmail.com>
2026-01-02 18:35:45 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,13 +1,1 @@
-import random
-PC = random.randint(1,5)
-           
-player = int(input("choose a random number from 1 to 5"))
 
-if PC == player:
-    print("you win")
-
-elif PC > player or PC < player:
-    print("you lose")
-
-else:
-    print("choose 1-5")
